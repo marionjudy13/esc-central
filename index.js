bd2701caaeb5b7598959ef44975018ead3b436c0
Marion Judy <marionejudy@gmail.com>
2024-06-04 17:54:33 -0500

    added mobile menu toggle functionality

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-// for IE10, .getElementsByClassName()[0];
 var toggle = document.querySelector(".toggle");
-var topNav = document.querySelector(".nav--top");
+var menu = document.querySelector(".menu");
+var menuLinks = document.querySelectorAll(".menu li a");
 
-// Listen for click event on toggle var
-toggle.addEventListener(
-  "click",
-  function () {
-    console.log("toggle clicked");
-    // toggle class "active" on topNav var
-    topNav.classList.toggle("active");
-  },
-  false
-);
+// Function to toggle menu and button image
+function toggleMenu() {
+  console.log("toggle clicked");
+  menu.classList.toggle("active");
+  // Toggle button image source based on menu state
+  if (menu.classList.contains("active")) {
+    toggle.src = "images/menu-close.svg"; // Change to the path of your close menu image
+  } else {
+    toggle.src = "images/menu.svg"; // Change to the path of your menu image
+  }
+}
+
+// Listen for click event on toggle variable
+toggle.addEventListener("click", toggleMenu);
+
+// Add click event listeners to all menu links
+menuLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    console.log("nav link clicked");
+    menu.classList.remove("active");
+    toggle.src = "images/menu.svg"; // Change to the path of your menu image
+  });
+});
+
+// Add event listener to close the menu when clicking outside of it
+document.addEventListener("click", function (event) {
+  if (!menu.contains(event.target) && !toggle.contains(event.target)) {
+    // If the clicked element is not inside the menu and not the toggle button, close the menu
+    menu.classList.remove("active");
+    toggle.src = "images/menu.svg"; // Change to the path of your menu image
+  }
+});
